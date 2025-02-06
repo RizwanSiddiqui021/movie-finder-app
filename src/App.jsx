ac85b6401449f2827544bb836d0a1ad3775c1f53
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-02-06 12:32:28 +0600

    made the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import Search from "./components/Search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <main>
       <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="/public/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
     </main>
   );
 };
---
+++ src/components/Search.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="/public/search.svg" alt="Search" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
+
+Search.propTypes = {
+  searchTerm: PropTypes.string,
+  setSearchTerm: PropTypes.string,
+};
